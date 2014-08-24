0b161b5ac42b2a8fb2faea965410b6667c985f49
Turnerj <git@turnerj.com>
2014-08-24 13:14:43 +0930

    Fixed test for identifying columns in a row

--- javascript/tablemate.tests.js
+++ javascript/tablemate.tests.js
@@ -1,11 +1,22 @@
 (function($)
 {
 	$.tablemate.tests = {
-		columnColours: ['#FF0000','#00FF00','#0000FF','#FFD800'],
+		columnColours: [
+			'#FF0000','#00FF00','#0000FF','#FFD800','#FF00DC',
+			'#00FFFF','#FF6A00','#00FF6E','#A100FF','#9BC3FF',
+			'#FFD093','#E2FF9B'
+		],
 		setup: function(Data)
 		{
-			//$.tablemate.tests.testColumnPosition.call(this, Data);
-			$.tablemate.tests.testColumnPosition2.call(this, Data);
+			var $table = $(this);
+			if ($table.hasClass('testColumnPosition'))
+				$.tablemate.tests.testColumnPosition.call(this, Data);
+			
+			if ($table.hasClass('testColumnPosition2'))
+				$.tablemate.tests.testColumnPosition2.call(this, Data);
+			
+			if ($table.hasClass('testColumnPosition3'))
+				$.tablemate.tests.testColumnPosition3.call(this, Data);
 		},
 		testColumnPosition: function(Data)
 		{
@@ -81,6 +92,79 @@
 					{
 						setSpannedRows.call(this, RowIndex, this.rowSpan, ColIndex);
 					}
+					
+					if (colourIndex >= colours.length)
+						colourIndex = 0;
+					
+					$cell.append('<div class="testBlock" style="background:'+colours[colourIndex]+';"></div>');
+					
+					colourIndex++;
+				});
+			});
+		},
+		testColumnPosition3: function(Data)
+		{
+			var $this = $(this), $rows = $(this.rows), columnCount = 0;
+			
+			//Setup cell data
+			$rows.each(function(RowIndex)
+			{
+				if (RowIndex == 0)
+				{
+					columnCount = this.cells.length;
+				}
+				
+				var $row = $(this), cells = [];
+				cells.length = columnCount;
+				
+				//$row.data('cells', $.makeArray(this.cells));
+				$row.data('cells', cells);
+			});
+			
+			//Set the spanned cells
+			$rows.each(function(RowIndex)
+			{
+				var $row = $(this);
+				$cells = $(this.cells);
+				$cells.each(function(ColIndex)
+				{
+					var cells = $row.data('cells'), trueIndex = 0;
+					for (var i = 0, l = cells.length; i < l; i++)
+					{
+						if (typeof cells[i] == 'undefined')
+						{
+							cells[i] = this;
+							trueIndex = i;
+							break;
+						}
+					}
+					
+					$row.data('cells', cells);
+					
+					if (this.rowSpan > 1)
+					{
+						for (var i = RowIndex, l = RowIndex + this.rowSpan; i < l; i++)
+						{
+							var $tmpRow = $rows.eq(i);
+							var cells = $tmpRow.data('cells');
+							
+							cells[trueIndex] = this;
+							$tmpRow.data('cells', cells);
+						}
+					}
+				});
+				
+				console.log($row.data('cells'), 'Row ' + RowIndex);
+			});
+			
+			//Set colour
+			$rows.each(function(RowIndex)
+			{
+				var colourIndex = 0, colours = $.tablemate.tests.columnColours;
+				var $row = $(this), $cells = $($row.data('cells'));
+				$cells.each(function()
+				{
+					var $cell = $(this);
 					
 					if (colourIndex >= colours.length)
 						colourIndex = 0;
